2496906d437727d85838fbed2ae0648fe7f89bb3
jat <jat1014@163.com>
2018-09-22 19:15:51 +0800

    设计1.0

---
+++ src/Arithmetic.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arithmetic {
+
+    List e = new ArrayList<String>();   // 用于存放题目
+    List a = new ArrayList<String>();   // 用于按题号顺序存放答案
+
+    static void main(String[] args) {
+        // 
+
+    }
+
+    static void Range(int num) {
+        // 用于控制数值范围，并生成题目
+
+    }
+
+    static String Change(int n, int d) {
+        // 将假分数转换为真分数，第一个参数为分子，第二个为分母
+
+        return ;
+    }
+
+    static void Ifexist(List s) {
+        // 用于检查是否有相同的题目，并处理
+
+    }
+
+    static void Save(List s) {
+        // 用于将题目存入当前目录下的Exercises.txt文件
+
+    }
+
+    static void Answer(List a) {
+        // 用于将题目答案存入当前目录下的Answer.txt文件
+
+    }
+
+    static void Check(File e, File a) {
+        // 用于对给定的题目文件和答案文件判断答案文件中的对错并统计
+    }
+}
