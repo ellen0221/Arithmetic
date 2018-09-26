1e80169414e72a3d72bb8e5012abad1b60e0ee66
cshao-library <cshao@gudt.edu.cn>
2018-09-26 15:23:49 +0800

    实现基本功能--文敏

--- src/Arithmetic.java
+++ src/Arithmetic.java
@@ -1,30 +1,55 @@
+package ari;
 import javax.security.auth.Subject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Arithmetic {
 
-    List e = new ArrayList<String>();   // ç”¨äºå­˜æ”¾é¢˜ç›®
-    List a = new ArrayList<String>();   // ç”¨äºæŒ‰é¢˜å·é¡ºåºå­˜æ”¾ç­”æ¡ˆ
-    static int  num = 10;    // é¢˜ç›®æ•°é‡ï¼Œé»˜è®¤ä¸º10é“
-    static int range = 0;   // æ•°å€¼èŒƒå›´
-    static String error = null;    // é”™è¯¯ä¿¡æ¯
-    static String efile = "src" + File.separator;   // ç»™å®šçš„é¢˜ç›®æ–‡ä»¶
-    static String afile = "src" + File.separator;   // ç»™å®šçš„ç­”æ¡ˆæ–‡ä»¶
+    List e = new ArrayList<String>();   // ÓÃÓÚ´æ·ÅÌâÄ¿
+    List a = new ArrayList<String>();   // ÓÃÓÚ°´ÌâºÅË³Ğò´æ·Å´ğ°¸
+    static int  num = 10;    // ÌâÄ¿ÊıÁ¿£¬Ä¬ÈÏÎª10µÀ
+    static int range = 0;   // ÊıÖµ·¶Î§
+    static String error = null;    // ´íÎóĞÅÏ¢
+    static String efile = "src" + File.separator;   // ¸ø¶¨µÄÌâÄ¿ÎÄ¼ş
+    static String afile = "src" + File.separator;   // ¸ø¶¨µÄ´ğ°¸ÎÄ¼ş
 
-    static void main(String[] argv) {
-        // æ ¹æ®åŠŸèƒ½å¤„ç†å‚æ•°
+    public static void main(String[] argv) {
+    	
+    	/*String nume=change(25,15);
+    	System.out.println(nume);
+    	//²âÊÔ¼Ù·ÖÊı×ª»»ÎªÕæ·ÖÊı*/
+    	
+    	/*List<String> s = new ArrayList<String>();
+		s.add("1+1=11");
+		s.add("1+1=11");
+		save(s);
+		//²âÊÔ´æÌâÄ¿*/
+    	
+    	/*List<String> a = new ArrayList<String>();
+		a.add("1+1=11");
+		a.add("1+1=11");
+		answer(a);
+		//²âÊÔ´æ´ğ°¸*/
+    	
+        // ¸ù¾İ¹¦ÄÜ´¦Àí²ÎÊı
         for (int i=0; i<argv.length; i++) {
             if (argv[i].equals("-r")) {
                 range = Integer.parseInt(argv[i+1]);
             } else if (argv[i].equals("-n"))
             {
                 num = Integer.parseInt(argv[i+1]);
-            } else if (argv[i].equals("-h")) {  // å¸®åŠ©ä¿¡æ¯
-                System.out.println("-r å‚æ•°: æ§åˆ¶é¢˜ç›®ä¸­æ•°å€¼ï¼ˆè‡ªç„¶æ•°ã€çœŸåˆ†æ•°å’ŒçœŸåˆ†æ•°åˆ†æ¯ï¼‰çš„èŒƒå›´ \nä¾‹å¦‚: Myapp.exe -r 10 å°†ç”Ÿæˆ10ä»¥å†…(ä¸åŒ…æ‹¬10)çš„å››åˆ™è¿ç®—é¢˜ç›®");
-                System.out.println("-n å‚æ•°: æ§åˆ¶ç”Ÿæˆé¢˜ç›®çš„ä¸ªæ•° \nä¾‹å¦‚: Myapp.exe -n 10  å°†ç”Ÿæˆ10ä¸ªé¢˜ç›®");
-                System.out.println("æ”¯æŒå¯¹ç»™å®šçš„é¢˜ç›®æ–‡ä»¶å’Œç­”æ¡ˆæ–‡ä»¶ï¼Œåˆ¤å®šç­”æ¡ˆä¸­çš„å¯¹é”™å¹¶è¿›è¡Œæ•°é‡ç»Ÿè®¡ å…·ä½“å‚æ•°å¦‚ä¸‹:\nMyapp.exe -e <exercisefile>.txt -a <answerfile>.txt");
+            } else if (argv[i].equals("-h")) {  // °ïÖúĞÅÏ¢
+                System.out.println("-r ²ÎÊı: ¿ØÖÆÌâÄ¿ÖĞÊıÖµ£¨×ÔÈ»Êı¡¢Õæ·ÖÊıºÍÕæ·ÖÊı·ÖÄ¸£©µÄ·¶Î§ \nÀıÈç: Myapp.exe -r 10 ½«Éú³É10ÒÔÄÚ(²»°üÀ¨10)µÄËÄÔòÔËËãÌâÄ¿");
+                System.out.println("-n ²ÎÊı: ¿ØÖÆÉú³ÉÌâÄ¿µÄ¸öÊı \nÀıÈç: Myapp.exe -n 10  ½«Éú³É10¸öÌâÄ¿");
+                System.out.println("Ö§³Ö¶Ô¸ø¶¨µÄÌâÄ¿ÎÄ¼şºÍ´ğ°¸ÎÄ¼ş£¬ÅĞ¶¨´ğ°¸ÖĞµÄ¶Ô´í²¢½øĞĞÊıÁ¿Í³¼Æ ¾ßÌå²ÎÊıÈçÏÂ:\nMyapp.exe -e <exercisefile>.txt -a <answerfile>.txt");
             } else if (argv[i].equals("-e")) {
                 efile = efile + argv[i+1];
             } else if (argv[i].equals("-a")) {
@@ -39,56 +64,283 @@ public class Arithmetic {
     }
 
     static boolean able(int n, int r) {
-        // ç”¨äºæ£€æŸ¥è¾“å…¥çš„é¢˜ç›®æ•°é‡ä¸æ•°å€¼èŒƒå›´æ˜¯å¦å¯å®ç°,å¹¶ä¿®æ”¹
+        // ÓÃÓÚ¼ì²éÊäÈëµÄÌâÄ¿ÊıÁ¿ÓëÊıÖµ·¶Î§ÊÇ·ñ¿ÉÊµÏÖ,²¢ĞŞ¸Ä
         if (n>10000) {
-            error = "æœ€å¤§é¢˜æ•°ä¸º10000";
+            error = "×î´óÌâÊıÎª10000";
             return false;
         } else if (n<=0) {
-            error = "é¢˜æ•°ä¸åˆæ³•";
+            error = "ÌâÊı²»ºÏ·¨";
             return false;
         } else if (range == 0) {
-            error = "è¯·ç»™å®šæ•°å€¼èŒƒå›´ï¼Œè¯¦ç»†æ“ä½œé€šè¿‡ -h æŸ¥çœ‹";
+            error = "Çë¸ø¶¨ÊıÖµ·¶Î§£¬ÏêÏ¸²Ù×÷Í¨¹ı -h ²é¿´";
             return false;
         } else if (range<0) {
-            error = "æ•°å€¼èŒƒå›´ä¸åˆæ³•";
+            error = "ÊıÖµ·¶Î§²»ºÏ·¨";
             return false;
         } else if (range<5) {
-            error = "æ•°å€¼èŒƒå›´è¿‡å°ï¼Œè‡³å°‘ä¸º5";
+            error = "ÊıÖµ·¶Î§¹ıĞ¡£¬ÖÁÉÙÎª5";
             return false;
         }
         return true;
     }
 
     static void subject(int n, int r) {
-        // ç”ŸæˆæŒ‡å®šæ•°é‡é¢˜ç›®
-        String[] e = {"+", "-", "x", "Ã·"};
+        // Éú³ÉÖ¸¶¨ÊıÁ¿ÌâÄ¿
+        String[] e = {"+", "-", "x", "¡Â"};
 
     }
 
     static void ifExist(List s) {
-        // ç”¨äºæ£€æŸ¥æ˜¯å¦æœ‰ç›¸åŒçš„é¢˜ç›®ï¼Œå¹¶å¤„ç†
+        // ÓÃÓÚ¼ì²éÊÇ·ñÓĞÏàÍ¬µÄÌâÄ¿£¬²¢´¦Àí
 
     }
+    
+  /*//½«ÖĞ×º±í´ïÊ½×ª»»³Éºó×º±í´ïÊ½
+    public static ArrayList transform(String prefix) {
+        //System.out.println("transform");
+        int i, len = prefix.length();
+        prefix=prefix+ '#';
+        Stack<Character> stack = new Stack<Character>();// ±£´æ²Ù×÷·ûµÄÕ»
+        stack.push('#');
+        ArrayList postfix = new ArrayList();
+       
+        for (i = 0; i < len + 1; i++) {
+            //System.out.println(i+" "+prefix.charAt(i));
+            if (Character.isDigit(prefix.charAt(i))) {
+                if (Character.isDigit(prefix.charAt(i+1))) {
+                    postfix.add(10 * (prefix.charAt(i)-'0') + (prefix.charAt(i+1)-'0'));
+                    i++;
+                } else {
+                    postfix.add((prefix.charAt(i)-'0'));
+                }
+            } else {
+                switch (prefix.charAt(i)) {
+                case '(':
+                    stack.push(prefix.charAt(i));
+                    break;
+                case ')':
+                    while (stack.peek() != '(') {
+                        postfix.add(stack.pop());
+                    }
+                    stack.pop();
+                    break;
+                default:// Ä¬ÈÏÇé¿öÏÂ:+ - * /
+                    while (stack.peek() != '#'
+                            && compare(stack.peek(), prefix.charAt(i))) {
+                        postfix.add(stack.pop());// ²»¶Ïµ¯Õ»£¬Ö±µ½µ±Ç°µÄ²Ù×÷·ûµÄÓÅÏÈ¼¶¸ßÓÚÕ»¶¥²Ù×÷·û
+                    }
+                    if (prefix.charAt(i) != '#') {// Èç¹ûµ±Ç°µÄ²Ù×÷·û²»ÊÇ'#'(½áÊø·û)£¬ÄÇÃ´Èë²Ù×÷·ûÕ»
+                        stack.push(prefix.charAt(i));// ×îºóµÄ±êÊ¶·û'#'ÊÇ²»ÈëÕ»µÄ
+                    }
+                    break;
+                }
+            }
+        }
+        return postfix;
+    }
 
-    static void check(File e, File a) {
-        // ç”¨äºå¯¹ç»™å®šçš„é¢˜ç›®æ–‡ä»¶å’Œç­”æ¡ˆæ–‡ä»¶åˆ¤æ–­ç­”æ¡ˆæ–‡ä»¶ä¸­çš„å¯¹é”™å¹¶ç»Ÿè®¡
+    //±È½ÏÔËËã·ûÖ®¼äµÄÓÅÏÈ¼¶
+    public static boolean compare(char peek, char cur) {
+        if (peek == '*'
+                && (cur == '+' || cur == '-' || cur == '/' || cur == '*')) {
+            return true;
+        } else if (peek == '/'
+                && (cur == '+' || cur == '-' || cur == '*' || cur == '/')) {
+            return true;
+        } else if (peek == '+' && (cur == '+' || cur == '-')) {
+            return true;
+        } else if (peek == '-' && (cur == '+' || cur == '-')) {
+            return true;
+        } else if (cur == '#') {
+            return true;
+        }
+        return false;
+    }
+    
+    //¼ÆËãºó×º±í´ïÊ½
+    public static double calculate(ArrayList postfix){
+        //System.out.println("calculate");
+        int i,size=postfix.size();
+        double res=0;
+        Stack<Double> stack_num=new Stack<Double>();
+        for(i=0;i<size;i++){
+            if(postfix.get(i).getClass()==Integer.class){
+            	//double c=(double) postfix.get(i);
+                stack_num.push(Double.parseDouble(String.valueOf( postfix.get(i))));                  
+                //System.out.println("push"+" "+(Integer)postfix.get(i));
+            }else{
+                //System.out.println((Character)postfix.get(i));
+            	double a=stack_num.pop();
+            	double b=stack_num.pop();
+                switch((Character)postfix.get(i)){
+                case '+':
+                    res=b+a;
+                    //System.out.println("+ "+a+" "+b);
+                    break;
+                case '-':
+                    res=b-a;
+                    //System.out.println("- "+a+" "+b);
+                    break;
+                case '*':
+                    res=b*a;
+                    //System.out.println("* "+a+" "+b);
+                    break;
+                case '/':
+                    res=b/a;
+                    //System.out.println("/ "+a+" "+b);
+                    break;
+                }
+                stack_num.push(res);
+                //System.out.println("push"+" "+res);
+            }
+        }
+        res=stack_num.pop();
+        //System.out.println("res "+" "+res);
+        return res;
+       //if(res==24){
+           // return true;
+      // }
+        //return false;
+    }*/
+
+
+  static void check(File e, File a,File g) {
+        // ÓÃÓÚ¶Ô¸ø¶¨µÄÌâÄ¿ÎÄ¼şºÍ´ğ°¸ÎÄ¼şÅĞ¶Ï´ğ°¸ÎÄ¼şÖĞµÄ¶Ô´í²¢Í³¼Æ
+    	try (BufferedReader exReader = new BufferedReader(new FileReader(e));
+                BufferedReader anReader = new BufferedReader(new FileReader(a));
+                BufferedWriter gradeWriter = new BufferedWriter(new FileWriter(g))
+           ) {
+               String ex, an;
+               int c = 0, w = 0;
+               StringBuilder correct = new StringBuilder("Correct: %d (");
+               StringBuilder wrong = new StringBuilder("Wrong: %d (");
+               while ((ex = exReader.readLine()) != null && (an = anReader.readLine()) != null) {
+                   int exPoint = ex.indexOf(".");
+                   int anPoint = an.indexOf(".");
+                   if (exPoint != -1 && anPoint != -1) {
+                       int i = Integer.valueOf(ex.substring(0,exPoint).trim());
+                       String expression = ex.substring(exPoint + 2);
+                       String answer = an.substring(anPoint + 2);
+                       if (expression.calculate(postfix).equals(answer.toString())) {
+                    	   /*ĞèÒªÄÃµ½¼ÆËãµÄ½á¹ûÕâ¸ö·½·¨²ÅÄÜÓÃ
+                    	   ÉÏÃæ×¢ÊÍµôµÄÊÇÎÒ´Ó±ğÈË²©¿ÍCopyÀ´µÄÏà¹Ø´úÂë*/
+                           c++;
+                           correct.append(" ").append(i);
+                           if (c % 20 == 0) {
+                               correct.append("\n");
+                           }
+                       } else {
+                           w++;
+                           wrong.append(" ").append(i);
+                           if (w % 20 == 0) {
+                               wrong.append("\n");
+                           }
+                       }
+                   }
+               }
+               gradeWriter.write(String.format(correct.append(" )\n").toString(),c));
+               gradeWriter.write(String.format(wrong.append(" )\n").toString(),w));
+               gradeWriter.flush();
+           } catch (IOException e1) {
+               e1.printStackTrace();
+           }
 
     }
 
     static String change(int n, int d) {
-        // å°†å‡åˆ†æ•°è½¬æ¢ä¸ºçœŸåˆ†æ•°ï¼Œç¬¬ä¸€ä¸ªå‚æ•°ä¸ºåˆ†å­ï¼Œç¬¬äºŒä¸ªä¸ºåˆ†æ¯
-
-        return ;
+        // ½«¼Ù·ÖÊı×ª»»ÎªÕæ·ÖÊı£¬µÚÒ»¸ö²ÎÊıÎª·Ö×Ó£¬µÚ¶ş¸öÎª·ÖÄ¸
+    	//¼Ù·ÖÊı×ªÕæ·ÖÊı·½·¨ Çó ×î´óÕû³ıÊıtemp+ÓàÊı/·ÖÄ¸
+    	int temp=1,min=n;
+        if(n%d==0){
+        	return Integer.toString(n/d);
+        }//·Ö×Ó·ÖÄ¸ÓàÊıÎª0·µ»Ø×ÔÈ»Êı
+        if(d<min){
+        	min=d;
+        }	        
+       for(int i=min;i>1;i--){
+        	if(n%i==0&&d%i==0){
+        		temp=i;
+        		break;
+        	}
+        }//Çó×î´ó¹«Ô¼Êıtemp
+        if(temp==1){
+        	if(n<d){
+        		return n+"/"+d;
+        	}//Èç¹û·Ö×ÓĞ¡ÓÚ·ÖÄ¸£¬Ö±½Ó·µ»Ø¸Ã·ÖÊı
+        	else{
+        		return n/d+"'"+n%d+"/"+d;
+        	}
+        }//·ÖÄ¸´óÓÚ·Ö×Ó·µ»ØÕæ·ÖÊı
+        else{
+        	if(n<d){
+		        return n/temp+"/"+d/temp;
+        	}//»¯¼ò×î´ó¹«Ô¼Êı
+        	else{
+        		return (n/temp)/(d/temp)+"'"+(n/temp)%(d/temp)+"/"+d/temp;
+        	}//»¯¼òÕæ·ÖÊıµÄ×î´ó¹«Ô¼Êı
+        }
     }
 
     static void save(List s) {
-        // ç”¨äºå°†é¢˜ç›®å­˜å…¥å½“å‰ç›®å½•ä¸‹çš„Exercises.txtæ–‡ä»¶
-
-    }
+        // ÓÃÓÚ½«ÌâÄ¿´æÈëµ±Ç°Ä¿Â¼ÏÂµÄExercises.txtÎÄ¼ş
+    	  File question = new File("./Exercises.txt");
+         if (!question.exists()) {
+                System.out.println("ÎÄ¼ş²»´æÔÚ£¬´´½¨ÎÄ¼ş: Exercises.txt" );
+                try {
+                    question.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("ÎÄ¼şÒÑ´æÔÚ£¬ÎÄ¼şÎª: Exercises.txt" );
+            }
+         FileWriter fw;
+    	try {
+    		fw = new FileWriter(question);
+    		BufferedWriter bw =new BufferedWriter(fw);
+    	    for(int i=0;i<s.size();i++){
+    	    		String t = i+1+". "+(String) s.get(i);
+    	        bw.write( t);
+    	        bw.newLine();
+    	    }
+    	    bw.close();
+    	} catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+        }
+    
 
     static void answer(List a) {
-        // ç”¨äºå°†é¢˜ç›®ç­”æ¡ˆå­˜å…¥å½“å‰ç›®å½•ä¸‹çš„Answer.txtæ–‡ä»¶
-
+        // ÓÃÓÚ½«ÌâÄ¿´ğ°¸´æÈëµ±Ç°Ä¿Â¼ÏÂµÄAnswer.txtÎÄ¼ş
+    	
+    		File answer = new File("./Answer.txt");
+         if (!answer.exists()) {
+                System.out.println("ÎÄ¼ş²»´æÔÚ£¬´´½¨ÎÄ¼ş: Answer.txt" );
+                try {
+                    answer.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("ÎÄ¼şÒÑ´æÔÚ£¬ÎÄ¼şÎª: Answer.txt" );
+            }
+         FileWriter fw;
+    	try {
+    		fw = new FileWriter(answer);
+    		BufferedWriter bw =new BufferedWriter(fw);
+    	    for(int i=0;i<a.size();i++){
+    	    		String t =i+1+". "+ (String) a.get(i);
+    	        bw.write( t);
+    	        bw.newLine();
+    	    }
+    	    bw.close();
+    	} catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+        }
     }
 
-}
+
+
